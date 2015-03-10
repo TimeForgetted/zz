998eebb3714a43d8b7e4dc0f50ca71d6576cbb71
XuYi <mast_yi@hotmail.com>
2015-03-10 11:28:48 +0800

    js/img/css 文件夹常量

---
+++ app/components/AppConstant.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xuyi
+ * Date: 15/3/10
+ * Time: 上午11:25
+ */
+
+namespace app\components;
+
+//由于css/js/img和app文件架同级
+class AppConstant {
+    const PATH_CSS = '/css/';
+    const PATH_JS  = '/js/' ;
+    const PATH_IMG = '/img/';
+}
